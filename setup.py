ee2a6016e521d51a16024bdf16240bddc4769e6f
Chris Gonnerman <chris@gonnerman.org>
2020-03-18 14:14:51 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ python_version = v[0] * 100 + v[1] * 10 + v[2]
 def dict_of(**kws): return kws
 
 # the minimal info, which Python 2.0's distutils can digest
-setup_args = dict_of(name="WConio",
+setup_args = dict_of(name="WConio2",
           version=this_version,
-          description="WConio",
+          description="WConio2",
           long_description="Windows Console I/O",
           author="Chris Gonnerman",
           author_email="chris@gonnerman.org",
           url="http://newcenturycomputers.net/projects/wconio.html",
-          py_modules=["WConio"],
+          py_modules=["WConio2"],
     )
 
 # provide more metadata if we have Python 2.1 (better distutils)
