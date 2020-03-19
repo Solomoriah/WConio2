d288c671600b0da76b8ce0831590705c2d391e72
Chris Gonnerman <chris@gonnerman.org>
2020-03-18 19:35:06 -0500

    Preparing for PyPI distribution

--- setup.py
+++ setup.py
@@ -1,37 +1,26 @@
 # Setup for WConio
-# Modified from samples by Alex Martelli.
 
-from distutils.core import setup, Extension
-import sys
+from distutils.core import setup
+setup(
+  name = 'WConio2',
+  py_modules = ['WConio2'],
+  version = '2.0',
+  license='MIT',
+  description="Windows Console I/O",
+  author = 'Chris Gonnerman',
+  author_email = 'chris@gonnerman.org',
+  url = 'https://github.com/Solomoriah/WConio2',
+  download_url = 'https://github.com/Solomoriah/WConio2/archive/v20.tar.gz', 
+  keywords = [ "windows", "console", "screen", ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+  ],
+)
 
-# version of this WConio package
-this_version = "2.0"
 
-# Determine Python version (tested on 2.0 and 2.1)
-v = sys.version_info
-python_version = v[0] * 100 + v[1] * 10 + v[2]
-
-# turn keyword-arguments into a directory object
-def dict_of(**kws): return kws
-
-# the minimal info, which Python 2.0's distutils can digest
-setup_args = dict_of(name="WConio2",
-          version=this_version,
-          description="WConio2",
-          long_description="Windows Console I/O",
-          author="Chris Gonnerman",
-          author_email="chris@gonnerman.org",
-          url="http://newcenturycomputers.net/projects/wconio.html",
-          py_modules=["WConio2"],
-    )
-
-# provide more metadata if we have Python 2.1 (better distutils)
-if python_version > 200:
-    setup_args.update(dict_of(
-          platforms="Win32",
-          keywords="console screen",
-    ))
-
-# and finally call the distutils' setup
-setup(**setup_args)
-
+# end of file.
