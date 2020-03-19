64af05b56674b345f83cf03ab54fc527289749ff
Chris Gonnerman <chris@gonnerman.org>
2020-03-18 19:42:52 -0500

    Fixes to the setup.py and README for distribution, no code changes.

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
 # Setup for WConio
 
+long_description = """
+WConio2.py
+----------
+
+This module is a pure Python reimplementation of my WConio module, and is
+generally API compatible with version 1.5.1 of that module.  WConio provides a
+color console interface for Windows applications using an interface based
+generally on the conio.h functions included with Turbo C 2.0.  While the
+original module depended on code written by Daniel Guerrero Miralles, this
+module includes none of his code.
+
+The conio.h functions don't map perfectly to Python, so I have taken some
+liberties.  In particular, the window() functionality is not provided, and
+screen coordinates are based at 0, 0 (logical for Python, but counter to the
+tradition of conio.h).
+
+If you are porting an application written for the original WConio, it's
+perfectly acceptable to say:
+
+import WConio2 as WConio
+"""
+
 from distutils.core import setup
 setup(
   name = 'WConio2',
@@ -7,6 +29,7 @@ setup(
   version = '2.0',
   license='MIT',
   description="Windows Console I/O",
+  long_description=long_description,
   author = 'Chris Gonnerman',
   author_email = 'chris@gonnerman.org',
   url = 'https://github.com/Solomoriah/WConio2',
